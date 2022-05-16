890e6741f0c850d38adecbba86562b3fa6cf0f62
puseyr <pusey.ronald@gmail.com>
2022-05-15 20:20:11 -0400

    Adding PS1 Script

---
+++ Extension cleaner.ps1
@@ -0,0 +1,9 @@
+﻿
+function deleteICA {
+    param (
+        $userPath = $env:USERPROFILE
+    )
+    Get-ChildItem -Path $userPath\Downloads *.ica | foreach { Remove-Item -Path $_.FullName }
+}
+
+Get-Command
